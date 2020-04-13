b86c0422ef1ba045d816459ecf57f1dc1dee4556
Stijn Vandendriessche <stijn.vandendriessche3@student.howest.be>
2020-04-13 09:14:58 +0200

    azertyuio

---
+++ Server/start.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+from Server.gui_server import ServerWindow
+
+
+def callback():
+    print("callback")
+    gui_server.afsluiten_server()
+    root.destroy()
+
+root = Tk()
+root.geometry("600x500")
+gui_server = ServerWindow(root)
+root.protocol("WM_DELETE_WINDOW", callback)
+root.mainloop()
