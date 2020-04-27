70c87bd52439145d4a546a11c40d7241a7443d00
Stijn Vandendriessche <stijn.vandendriessche3@student.howest.be>
2020-04-27 20:52:30 +0200

    gui

--- client/gui_dashboard.py
+++ client/gui_dashboard.py
@@ -2,6 +2,10 @@ from tkinter import *
 from tkinter import messagebox
 import jsonpickle
 from tkinter.ttk import *
+from tkinter import scrolledtext
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
 
 class Dashboard(Frame):
 
@@ -9,6 +13,11 @@ class Dashboard(Frame):
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
+        self.article_title = StringVar()
+        self.article_title.set("Titel")
+        self.article_text = StringVar()
+        self.article_text.set("Text")
+        self.Image = None
         self.master = master
         self.init_window()
 
@@ -31,30 +40,106 @@ class Dashboard(Frame):
         self.btnSearch = Button(self, text="Zoek!", command=self.zoeken)
         self.btnSearch.grid(row=1, column=2, pady=(32, 32), sticky=N + S + E + W)
 
-        #Label(self, text="Zoeken:", font=("Helvetica", 32, "bold italic")).grid(row=0)
+        self.btnStats = Button(self, text="Statistieken", command=self.zoeken)
+        self.btnStats.grid(row=1, column=4, padx=(372, 16), pady=(32, 32), sticky=E)
 
-        self.zoeken()
+        self.Title = Label(self, textvariable=self.article_title, font=("Arial", 24, "bold italic")).grid(row=2, column=1, columnspan=4)
+        self.Text = Label(self, textvariable=self.article_text, font=("Arial",8)).grid(row=3,column=1,columnspan=4)
+        #self.Image = Label(self).grid(row=4,column=1,columnspan=4, image=self.Image)
+
+        self.btnRandom = Button(self, text="Willekeurig nieuw artikel", command=self.get_random)
+        self.btnRandom.grid(row=4, column=1, padx=(16, 16), pady=(32, 32), sticky=W)
+
+        self.get_random()
 
     def clearPlaceholder(self,a):
         self.entSearch.config(foreground="black")
         self.entSearch.delete(0, 'end')
     def addPlaceholder(self,a):
         self.entSearch.config(foreground="gray")
-        self.entSearch.delete(0, 'end')
-        self.entSearch.insert(0, 'Zoek een artikel op titel')
+        if not self.entSearch.get():
+            self.entSearch.delete(0, 'end')
+            self.entSearch.insert(0, 'Zoek een artikel op titel')
 
-    def zoeken(self, title="Mikhail Saakashvili quits as Odessa governor"):
+    def zoeken(self):
+        title = self.entSearch.get()
+        print(title)
         self.master.my_writer_obj.write("title\n")
         self.master.my_writer_obj.write("%s\n" % title)
         self.master.my_writer_obj.flush()
         answer = self.master.my_writer_obj.readline().rstrip('\n')
+        print(answer)
         art = jsonpickle.decode(answer)
-        print(art.img)
+        self.article_title.set("")
+        self.article_text.set("")
+        d = 1
+        for i in art.title:
+            if d >= 45:
+                if i == " ":
+                    self.article_title.set(self.article_title.get() + "\n")
+                    d = 1
+                else:
+                    self.article_title.set(self.article_title.get() + i)
+            else:
+                self.article_title.set(self.article_title.get() + i)
+            d += 1
 
-        # sample code van img in tkinter
-        # resp = requests.get(art.img)
-        # load = Image.open(BytesIO(resp.content))
-        # render = ImageTk.PhotoImage(load)
-        # img = Label(image=render)
-        # img.image = render
-        # img.place(x=0,y=0)
+        d = 1
+        for i in art.text:
+            if i == "\n":
+                d = 1
+                self.article_text.set(self.article_text.get() + i)
+                continue
+            else:
+                if d >= 180:
+                    if i == " ":
+                        self.article_text.set(self.article_text.get() + "\n")
+                        d = 1
+                    else:
+                        self.article_text.set(self.article_text.get() + i)
+                else:
+                    self.article_text.set(self.article_text.get() + i)
+            d += 1
+
+    def get_random(self):
+        self.master.my_writer_obj.write("random\n")
+        self.master.my_writer_obj.flush()
+        answer = self.master.my_writer_obj.readline().rstrip('\n')
+        art = jsonpickle.decode(answer)
+        self.article_title.set("")
+        self.article_text.set("")
+        d = 1
+        for i in art.title:
+            if d >= 45:
+                if i == " ":
+                    self.article_title.set(self.article_title.get() + "\n")
+                    d = 1
+                else:
+                    self.article_title.set(self.article_title.get() + i)
+            else:
+                self.article_title.set(self.article_title.get() + i)
+            d += 1
+
+        d = 1
+        for i in art.text:
+            if i == "\n":
+                d = 1
+                self.article_text.set(self.article_text.get() + i)
+                continue
+            else:
+                if d >= 180:
+                    if i == " ":
+                        self.article_text.set(self.article_text.get() + "\n")
+                        d = 1
+                    else:
+                        self.article_text.set(self.article_text.get() + i)
+                else:
+                    self.article_text.set(self.article_text.get() + i)
+            d+=1
+
+        #sample code van img in tkinter
+        #resp = requests.get(art.img)
+        #load = Image.open(BytesIO(resp.content))
+        #render = ImageTk.PhotoImage(load)
+        #self.Image.configure(image=render)
+        #self.Image.image = render
